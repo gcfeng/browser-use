f59fcb82fb6685401492973f4813562837cd2fbb
gcfeng <fengguocai@bytedance.com>
2025-04-08 21:15:12 +0800

    feat: add cozeloop

--- browser_use/agent/prompts.py
+++ browser_use/agent/prompts.py
@@ -129,7 +129,7 @@ Interactive elements from top layer of the current page inside the viewport:
 					{'type': 'text', 'text': state_description},
 					{
 						'type': 'image_url',
-						'image_url': {'url': f'data:image/png;base64,{self.state.screenshot}'},  # , 'detail': 'low'
+						'image_url': {'url': f'data:image/jpeg;base64,{self.state.screenshot}'},  # , 'detail': 'low'
 					},
 				]
 			)
